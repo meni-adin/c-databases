92602d778b127354a94190e35084fcc4c42a8073
Menachem Adin <meniadin@gmail.com>
2024-09-09 22:39:33 +0300

    test

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,9 +1,5 @@
 function(set_target_c_compiler_flags target)
-    if(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-        message(STATUS "I'm a C compiler ${CMAKE_C_COMPILER_ID}")
-    endif()
-
-    if((${CMAKE_C_COMPILER_ID} STREQUAL "AppleClang") OR (${CMAKE_C_COMPILER_ID} STREQUAL "GNU"))
+    if((CMAKE_C_COMPILER_ID STREQUAL "AppleClang") OR (CMAKE_C_COMPILER_ID STREQUAL "GNU"))
         target_compile_options(${target} PRIVATE
             -Werror
             -Wall
@@ -17,15 +13,15 @@ function(set_target_c_compiler_flags target)
             -Wdouble-promotion
             -Wimplicit-fallthrough
         )
-        if(${CMAKE_C_COMPILER_ID} STREQUAL "AppleClang")
+        if(CMAKE_C_COMPILER_ID STREQUAL "AppleClang")
         endif()
-        if(${CMAKE_C_COMPILER_ID} STREQUAL "GNU")
+        if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
             target_compile_options(${target} PRIVATE
                 -Wformat-signedness
             )
         endif()
-    elseif(${CMAKE_C_COMPILER_ID} STREQUAL "MSVC")
-        message(FATAL_ERROR "Missing compiler flags for ${CMAKE_C_COMPILER_ID}")
+    elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
+        message(WARNING "Missing compiler flags for ${CMAKE_C_COMPILER_ID}")
     else()
         message(FATAL_ERROR "Unknown C compiler: ${CMAKE_C_COMPILER_ID}")
     endif()
