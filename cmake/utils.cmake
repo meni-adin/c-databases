626e5b46252bc3b89328be25d78837c1a9a917db
Menachem Adin <meniadin@gmail.com>
2024-09-09 21:20:08 +0300

    Add compiler warnings

---
+++ cmake/utils.cmake
@@ -0,0 +1,22 @@
+function(set_target_c_compiler_flags target)
+    if((${CMAKE_C_COMPILER_ID} STREQUAL "AppleClang") OR (${CMAKE_C_COMPILER_ID} STREQUAL "GNU"))
+        target_compile_options(${target} PRIVATE
+            -Werror
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wvla
+            -Wformat
+        )
+        if(${CMAKE_C_COMPILER_ID} STREQUAL "AppleClang")
+        endif()
+        if(${CMAKE_C_COMPILER_ID} STREQUAL "GNU")
+            target_compile_options(${target} PRIVATE
+                -Wformat-signedness
+            )
+        endif()
+    elseif(${CMAKE_C_COMPILER_ID} STREQUAL "visual studio") # check exact name
+    else()
+        message(FATAL_ERROR "Unknown C compiler: ${CMAKE_C_COMPILER_ID}")
+    endif()
+endfunction()
