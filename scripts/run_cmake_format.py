36ab70158ffee8efe57e61fa5ec908aca384c11f
Menachem Adin <meniadin@gmail.com>
2024-09-12 08:59:21 +0300

    Edit settings for cmake-format

---
+++ scripts/run_cmake_format.py
@@ -0,0 +1,20 @@
+import utils
+
+CLANG_FORMAT_CFG = utils.PROJECT_DIR/'.cmake-format.py'
+
+directories = [
+    utils.PROJECT_DIR/'cmake',
+    utils.PROJECT_DIR/'src',
+    utils.PROJECT_DIR/'test',
+]
+
+cmake_files = []
+cmake_files.append(utils.PROJECT_DIR/'CMakeLists.txt')
+for directory in directories:
+    cmake_files.extend(directory.rglob("CMakeLists.txt"))
+    cmake_files.extend(directory.rglob("*.cmake"))
+
+for file in cmake_files:
+    print(f'Running cmake-format on {file}')
+    command = f'cmake-format -i {file}'
+    utils.run_command(command, shell=True, check=True)
