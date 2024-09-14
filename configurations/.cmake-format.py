c3d71b8a2a158ec31e324978fb5873cbf5d89da3
Menachem Adin <meniadin@gmail.com>
2024-09-14 16:09:03 +0300

    Edit configurations

--- configurations/.cmake-format.py
+++ configurations/.cmake-format.py
@@ -1,2 +1,7 @@
-line_width = 120
-tab_size = 4
+# -----------------------------
+# Options affecting formatting.
+# -----------------------------
+with section("format"):
+    line_width = 120
+    tab_size = 4
+    use_tabchars = False
