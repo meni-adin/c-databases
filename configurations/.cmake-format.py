3a12d4a6b3cd31d72130dab1142e4b686e7d2112
Menachem Adin <meniadin@gmail.com>
2024-09-14 16:55:30 +0300

    Edit configurations

--- configurations/.cmake-format.py
+++ configurations/.cmake-format.py
@@ -2,6 +2,9 @@
 # Options affecting formatting.
 # -----------------------------
 with section("format"):
-    line_width = 120
+    line_width = 140
     tab_size = 4
     use_tabchars = False
+    max_subgroups_hwrap = 1
+    dangle_parens = False
+    always_wrap_to_list = True
